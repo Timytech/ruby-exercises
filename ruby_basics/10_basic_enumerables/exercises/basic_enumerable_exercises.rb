2f6cf9d0240c44daece975a89fb2f88011154247
Timytech <timpiano699@gmail.com>
2023-08-30 13:49:24 +0000

    Complete ruby-exercises basic-enumerable

--- ruby_basics/10_basic_enumerables/exercises/basic_enumerable_exercises.rb
+++ ruby_basics/10_basic_enumerables/exercises/basic_enumerable_exercises.rb
@@ -1,26 +1,54 @@
 def display_current_inventory(inventory_list)
   # use #each to iterate through each item of the inventory_list (a hash)
   # use puts to output each list item "<key>, quantity: <value>" to console
+  inventory_list.each do |key, value|
+    puts "#{key}, quantity: #{value}"
+  end
 end
+fruit = { apples: 1, bananas: 3, oranges: 7 }
+display_current_inventory(fruit)
+
 
 def display_guess_order(guesses)
   # use #each_with_index to iterate through each item of the guesses (an array)
   # use puts to output each list item "Guess #<number> is <item>" to console
   # hint: the number should start with 1
+  guesses.each_with_index do |item, number|
+    puts "Guess ##{number + 1} is #{item}"
+  end
 end
+guesses = ['cookies', 'cake', 'ice cream']
+display_guess_order(guesses)
+
 
 def find_absolute_values(numbers)
   # use #map to iterate through each item of the numbers (an array)
   # return an array of absolute values of each number
+  numbers.map { |num| num.abs }
 end
+numbers = [0, -7, 14, -21]
+numbers = [-3.14, 6.28, -9.42]
+p find_absolute_values(numbers)
+
 
 def find_low_inventory(inventory_list)
   # use #select to iterate through each item of the inventory_list (a hash)
   # return a hash of items with values less than 4
+inventory_list.select { |item, value| value < 4}
 end
+fruit = { apples: 1, peaches: 4, bananas: 3, oranges: 7 }
+find_low_inventory fruit
+
 
 def find_word_lengths(word_list)
   # use #reduce to iterate through each item of the word_list (an array)
   # return a hash with each word as the key and its length as the value
   # hint: look at the documentation and review the reduce examples in basic enumerable lesson
+  word_list.reduce(Hash.new(0)) do |new_list, word|
+    new_list[word] += word.to_s.length
+    new_list
+  end
 end
+animals = ['cat', 'horse', 'rabbit', 'deer']
+p find_word_lengths animals
+
